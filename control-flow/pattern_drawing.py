9691fd8567227a30e5ecaf80c08f1731798cd56c
Martinecx <martin254bria@gmail.com>
2025-08-10 04:47:13 -0800

    Add ALX-compliant control-flow task solutions

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,10 +1,14 @@
-# Get size from user
+# Ask the user for size
 size = int(input("Enter the size of the pattern: "))
 
-# Initialize counter
-i = 1
+# Initialize counters
+i = 0
 
-# While loop to draw the pattern
-while i <= size:
-    print("*" * i)  # Draw pattern line with i stars
+# While loop for rows
+while i < size:
+    j = 0
+    while j < size:
+        print("*", end="")
+        j += 1
+    print()  # Move to the next line
     i += 1
